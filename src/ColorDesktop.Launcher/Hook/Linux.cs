f8a3db5e45c451af33c805ddeb981d84736ec1d6
Color_yr <402067010@qq.com>
2024-10-12 18:56:03 +0800

    up use desktop

--- src/ColorDesktop.Launcher/Hook/Linux.cs
+++ src/ColorDesktop.Launcher/Hook/Linux.cs
@@ -17,25 +17,45 @@ public class Linux
         {
             try
             {
-                if (!File.Exists("/etc/systemd/user/colordesktop.service"))
-                {
-                    {
-                        var assm = Assembly.GetExecutingAssembly();
-                        using var item = assm.GetManifestResourceStream("ColorDesktop.Launcher.Resource.linux.service")!;
-                        using var file = File.Open(Program.RunDir + "colordesktop.service", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
-                        item.CopyTo(file);
-                    }
-                    Run("cp " + Program.RunDir + "colordesktop.service" + " /etc/systemd/user/colordesktop.service");
-                    Run("systemctl --user daemon-reload");
-                }
+                // if (!File.Exists("/etc/systemd/user/colordesktop.service"))
+                // {
+                //     {
+                //         var assm = Assembly.GetExecutingAssembly();
+                //         using var item = assm.GetManifestResourceStream("ColorDesktop.Launcher.Resource.linux.service")!;
+                //         using var file = File.Open(Program.RunDir + "colordesktop.service", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                //         item.CopyTo(file);
+                //     }
+                //     Run("pkexec","cp " + Program.RunDir + "colordesktop.service" + " /etc/systemd/user/colordesktop.service");
+                //     Run("systemctl", "--user daemon-reload");
+                // }
+
+                // if (start)
+                // {
+                //     Run("systemctl", "--user enable colordesktop.service");
+                // }
+                // else
+                // {
+                //     Run("systemctl", "--user disable colordesktop.service");
+                // }
 
                 if (start)
                 {
-                    Run("systemctl --user enable colordesktop.service");
+                    if (!File.Exists("/etc/xdg/autostart/colordesktop.desktop"))
+                    {
+                        var assm = Assembly.GetExecutingAssembly();
+                        using var item = assm.GetManifestResourceStream("ColorDesktop.Launcher.Resource.linux.desktop")!;
+                        using var file = File.Open(Program.RunDir + "colordesktop.desktop", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                        item.CopyTo(file);
+                    }
+
+                    Run("pkexec","cp " + Program.RunDir + "colordesktop.desktop" + " /etc/xdg/autostart/colordesktop.desktop");
                 }
                 else
                 {
-                    Run("systemctl --user disable colordesktop.service");
+                    if (File.Exists("/etc/xdg/autostart/colordesktop.desktop"))
+                    {
+                        Run("pkexec","rm /etc/xdg/autostart/colordesktop.desktop");
+                    }
                 }
             }
             catch (Exception e)
@@ -45,9 +65,9 @@ public class Linux
         });
     }
 
-    private static void Run(string cmd)
+    private static void Run(string pg, string cmd)
     {
-        var p = Process.Start("pkexec", cmd);
+        var p = Process.Start(pg, cmd);
         p.WaitForExit();
     }
 }
